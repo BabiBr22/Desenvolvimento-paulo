0bd08d1e2e87d75b43f5e0f963bfca12b030e090
BÁRBARA COELHO RIOS <barbara_rios@estudante.sesisenai.org.br>
2024-10-02 21:46:48 -0300

    aaa

---
+++ blueticket/server/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3001;
+
+// Serve arquivos estáticos da pasta "public"
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Rota principal
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Inicia o servidor
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+});
